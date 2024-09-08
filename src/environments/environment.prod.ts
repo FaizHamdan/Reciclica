446ce3a6f492961bb6450632a81327de3d348d15
FaizHamdan <hamdankewer@gmail.com>
2024-09-09 01:15:32 +0700

    Perbaikan Eror

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,11 @@
 export const environment = {
-  production: true
-
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCyeieFcHLIBCHx-HpaaWK2ob8AQOhz16A",
+    authDomain: "reciclica-c9e6d.firebaseapp.com",
+    projectId: "reciclica-c9e6d",
+    storageBucket: "reciclica-c9e6d.appspot.com",
+    messagingSenderId: "193325537563",
+    appId: "1:193325537563:web:dc62acc3cc9dafa1eb1bbe"
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,25 +1,11 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  firebaseConfig: {
-    apiKey: "AIzaSyB5v7dTawO32KDqSHn-3hsCFwj4mrCCGlQ",
-    authDomain: "reciclica-daa26.firebaseapp.com",
-    projectId: "reciclica-daa26",
-    storageBucket: "reciclica-daa26.appspot.com",
-    messagingSenderId: "839955787309",
-    appId: "1:839955787309:web:59784cf0ef7e2aaf1f92a4"
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCyeieFcHLIBCHx-HpaaWK2ob8AQOhz16A",
+    authDomain: "reciclica-c9e6d.firebaseapp.com",
+    projectId: "reciclica-c9e6d",
+    storageBucket: "reciclica-c9e6d.appspot.com",
+    messagingSenderId: "193325537563",
+    appId: "1:193325537563:web:dc62acc3cc9dafa1eb1bbe"
   },
-  production: false
-
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
