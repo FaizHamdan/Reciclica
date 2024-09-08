7f586731c26ebf5f855edfff375fd84f7d05b2e1
FaizHamdan <hamdankewer@gmail.com>
2024-09-08 23:46:54 +0700

    Acara 3 Fix

--- src/store/AppState.ts
+++
@@ -1,9 +0,0 @@
-import { LoadingState } from './loading/LoadingState';
-import { LoginState } from './login/LoginState';
-import { RegisterState } from './register/RegisterState';
-
-export interface AppState {
-  loading: LoadingState;
-  login: LoginState;
-  register: RegisterState;
-}
